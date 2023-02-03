6eb1dd1395a46c2288b92fbc703ae36d7114cb12
Susanne Arnetz Linder <susanne.arnetzlinder@medieinstitutet.se>
2023-02-03 16:37:58 +0100

    add first test in movieApp

--- src/ts/__test__/movieApp.test.ts
+++ src/ts/__test__/movieApp.test.ts
@@ -16,9 +16,23 @@
  describe("testing init function", () => {
     test("handleSubmit() is beeing called correctly", () => {
         //Arrange
+        document.body.innerHTML= `
+        <form id="searchForm">
+            <input type="text" id="searchText" placeholder="Skriv titel här" />
+            <button type="submit" id="search">Sök</button>
+        </form>
+        `;
+
+        let spyHandleSubmit = jest.spyOn(movieApp, "handleSubmit").mockReturnValue(new Promise<void>((resolve)=> {
+            resolve();
+        }));
 
         //Act
+        movieApp.init();
+        (document.getElementById("searchForm") as HTMLFormElement).submit();
 
         //Assert
-    })
- })
+        expect(spyHandleSubmit).toHaveBeenCalled();
+        spyHandleSubmit.mockRestore();
+    });
+ });
