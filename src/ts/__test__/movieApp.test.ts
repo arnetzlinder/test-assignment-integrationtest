2980f91543b3713e1cd471d98aa6b3a5d9ba4f72
Susanne Arnetz Linder <susanne.arnetzlinder@medieinstitutet.se>
2023-02-03 14:27:13 +0100

    change mock for movieser. and  test for movieApp

--- src/ts/__test__/movieApp.test.ts
+++ src/ts/__test__/movieApp.test.ts
@@ -1,3 +1,24 @@
 /**
  *@jest-environment jsdom
  */
+
+ import * as movieApp from "../movieApp";
+ import { getData } from "../services/movieservice";
+ import { testData } from "../services/__mocks__/movieservice";
+ import { IMovie } from "../models/Movie";
+
+ beforeEach(() => {
+    document.body.innerHTML = ``;
+ });
+ 
+ jest.mock("../services/movieservice");
+
+ describe("testing init function", () => {
+    test("handleSubmit() is beeing called correctly", () => {
+        //Arrange
+
+        //Act
+
+        //Assert
+    })
+ })
--- src/ts/__test__/movieservice.test.ts
+++ src/ts/__test__/movieservice.test.ts
@@ -2,32 +2,28 @@
  *@jest-environment jsdom
  */
 
-import * as movieservice from "../services/movieservice";
+import { getData } from "../services/movieservice";
 import { testData } from "../services/__mocks__/movieservice";
 import { IMovie } from "../models/Movie";
 
-
-jest.mock("axios", () => ({
-    get: async (url:string) => { 
-            if (!url.endsWith("error")) {
-                let returnvalue = {data: {Search: testData}, status: 200}
-                console.log(returnvalue);
-                return returnvalue;
+jest.mock("axios", () => {
+    get: async (url:string) => {
+        return new Promise((resolve, reject) => {
+            if(url.endsWith("error")) {
+                reject([]);
             } else {
-                throw new Error("Meh");
+                resolve({ data : {Search: testData}});
             }
-        }
-    
-}));
-
-
+        })
+    }
+});
 
 
 test("test för att se att data hämtas rätt", async () => {
     //Arrange
 
     //Act
-    let dataTest: IMovie[] = await movieservice.getData("Titanic");
+    let dataTest: IMovie[] = await getData("Titanic");
     console.log(dataTest);
     //Assert
     expect(dataTest.length).toBe(3);
@@ -35,8 +31,12 @@ test("test för att se att data hämtas rätt", async () => {
 });
 
 test("test för att se när kod fallerar", async () => {
-    let dataTest: IMovie[] = await movieservice.getData("error");
-    console.log(dataTest);
-    //Assert
-    expect(dataTest.length).toBe(0);
+
+    try {
+        let dataTest: IMovie[] = await getData("error");
+    }
+    catch(error: any) {
+        expect(error.length).toBe(0);
+        expect(error).toReturnWith([]);
+    }
 })
